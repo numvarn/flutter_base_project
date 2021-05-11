a360f8410215854b4aac54093566b3a4b487bb9f
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2021-05-11 21:51:46 +0700

    complete

--- lib/screens/gallery/gallery_screen.dart
+++ lib/screens/gallery/gallery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:base_project/components/img_cached_gallery_container.dart';
+import 'package:base_project/constants.dart';
 import 'package:base_project/data/image_network.dart';
 import 'package:flutter/material.dart';
 
@@ -13,30 +14,54 @@ class _GalleryScreenState extends State<GalleryScreen> with AutomaticKeepAliveCl
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: GridView.count(
-            crossAxisCount: 3,
-            mainAxisSpacing: 1.0,
-            crossAxisSpacing: 6.0,
-            padding: const EdgeInsets.all(10.0),
-            children: List.generate(galleryList.length, (index) {
-              return Card(
-                semanticContainer: true,
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Gallery",
+                style: header2Style,
+              ),
+              TextButton(
+                child: Text(
+                  "See All",
+                  style: header2Style,
                 ),
-                elevation: 1,
-                child: CachedImageGalleryContainer(
-                  imgUrl: "${galleryList[index]}",
-                ),
-              );
-            }),
+                onPressed: () {},
+              ),
+            ],
           ),
-        ),
-      ],
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 1.0,
+              crossAxisSpacing: 6.0,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(0.0),
+              children: List.generate(galleryList.length, (index) {
+                return Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 1,
+                  child: CachedImageGalleryContainer(
+                    imgUrl: "${galleryList[index]}",
+                  ),
+                );
+              }),
+            ),
+          ),
+          Container(),
+        ],
+      ),
     );
   }
 }
