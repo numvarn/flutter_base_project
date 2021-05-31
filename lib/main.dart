9fb9ae9c8f54d1820d153587587b67d448333b6f
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2021-06-01 03:00:32 +0700

    add initialize FlutterFire

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,46 @@ import 'validation/signup_validation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      print("Error $_error");
+    }
+    if (!_initialized) {
+      print("initialize FlutterFire...");
+    }
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<SignupValidation>(create: (_) => SignupValidation()),
